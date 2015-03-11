6e3df7c86e8bd6735eeb66a6bd8ee8a47eb4af0c
Emily Zhao <emily@dropbox.com>
2015-03-10 21:23:53 -0700

    Push notification on request changes

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,24 @@
-
 // Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
 Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world! Emily is testing");
+    response.success("Hello world! Emily is testing 4");
+});
+
+Parse.Cloud.afterSave("Request", function(request) {
+    var recipients = new Parse.Query(Parse.Installation);
+    var message = "";
+
+    if (request.object.get("approved")) {
+	recipients.equalTo("username", request.object.get("from"));
+	message = "Your contact request to " + request.object.get("to") + " was approved!";
+    } else {
+	recipients.equalTo("username", request.object.get("to"));
+	message = "You got a contact request from " + request.object.get("from");
+    }
+
+    Parse.Push.send({
+	where: recipients,
+	data: {
+	    alert: message
+	}
+    });
 });
