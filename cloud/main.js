a0f738ee308ed684b035e5f8ef6031f1624a210d
rtteal <rtteal@gmail.com>
2015-03-14 13:48:08 -0700

    new before delete method for declining requests

--- cloud/main.js
+++ cloud/main.js
@@ -68,24 +68,38 @@ Parse.Cloud.beforeSave("Request", function(request, response) {
     }
 });
 
-/*
-Parse.Cloud.afterSave("Request", function(request) {
-    var recipients = new Parse.Query(Parse.Installation);
-    var message = "";
-
-    if (request.object.get("approved")) {
-	recipients.equalTo("username", request.object.get("from"));
-	message = "Your contact request to " + request.object.get("to") + " was approved!";
-    } else {
-	recipients.equalTo("username", request.object.get("to"));
-	message = "You got a contact request from " + request.object.get("from");
-    }
-
-    Parse.Push.send({
-	where: recipients,
-	data: {
-	    alert: message
-	}
+Parse.Cloud.beforeDelete("Request", function(request, response) {
+    Parse.Cloud.useMasterKey(); // allows us to update the user table without being logged in
+    var to = request.object.get("to");
+    var contactObj = request.object.get("fromUser");
+    contactObj.fetch({
+        success: function(contact) {
+            var userQuery = new Parse.Query(Parse.User);
+            userQuery.equalTo("username", to);
+            userQuery.find({
+                success: function(user) {
+                    var theUser = user[0];
+                    var relation = theUser.relation("contacts");
+                    relation.remove(contact);
+                    theUser.save(null, {
+                        success: function(theUser) {
+                            response.success();
+                        },
+                        error: function(theUser, error) {
+                            console.log("Error: " + error.message);
+                            response.error(error.message);
+                        }
+                    });
+                },
+                error: function(user, error) {
+                    console.log("Error: " + error.message);
+                    response.error(error.message);
+                }
+            });
+        },
+        error: function(contact, error) {
+            console.log("Error: " + error.message);
+            response.error(error.message);
+        }
     });
 });
-*/
