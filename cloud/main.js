6f42ef5b67822a46997f0158fd40c574d244ac80
rtteal <rtteal@gmail.com>
2015-03-14 13:41:00 -0700

    new method for accepting or declining requests

--- cloud/main.js
+++ cloud/main.js
@@ -1,8 +1,74 @@
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-Parse.Cloud.define("hello", function(request, response) {
-    response.success("Hello world! Emily is testing 4");
+Parse.Cloud.beforeSave("Request", function(request, response) {
+    Parse.Cloud.useMasterKey(); // allows us to update the user table without being logged in
+    var to = request.object.get("to");
+    var contactObj = request.object.get("fromUser");
+
+    if (request.object.get("approved") === true) {
+        contactObj.fetch({
+            success: function(contact) {
+                var userQuery = new Parse.Query(Parse.User);
+                userQuery.equalTo("username", to);
+                userQuery.find({
+                    success: function(user) {
+                        var theUser = user[0];
+                        var relation = theUser.relation("contacts");
+                        relation.add(contact);
+                        theUser.save(null, {
+                            success: function(theUser) {
+                                response.success();
+                            },
+                            error: function(theUser, error) {
+                                console.log("Error: " + error.message);
+                                response.error(error.message);
+                            }
+                        });
+                    },
+                    error: function(user, error) {
+                        console.log("Error: " + error.message);
+                        response.error(error.message);
+                    }
+                });
+            },
+            error: function(contact, error) {
+                console.log("Error: " + error.message);
+                response.error(error.message);
+            }
+        });
+    } else {
+        contactObj.fetch({
+            success: function(contact) {
+                var userQuery = new Parse.Query(Parse.User);
+                userQuery.equalTo("username", to);
+                userQuery.find({
+                    success: function(user) {
+                        var theUser = user[0];
+                        var relation = theUser.relation("contacts");
+                        relation.remove(contact);
+                        theUser.save(null, {
+                            success: function(theUser) {
+                                response.success();
+                            },
+                            error: function(theUser, error) {
+                                console.log("Error: " + error.message);
+                                response.error(error.message);
+                            }
+                        });
+                    },
+                    error: function(user, error) {
+                        console.log("Error: " + error.message);
+                        response.error(error.message);
+                    }
+                });
+            },
+            error: function(contact, error) {
+                console.log("Error: " + error.message);
+                response.error(error.message);
+            }
+        });
+    }
 });
 
+/*
 Parse.Cloud.afterSave("Request", function(request) {
     var recipients = new Parse.Query(Parse.Installation);
     var message = "";
@@ -22,3 +88,4 @@ Parse.Cloud.afterSave("Request", function(request) {
 	}
     });
 });
+*/
